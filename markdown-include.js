bd3c35d806eb3e530a88a9a43a4a43da82db1e1d
David Crossley <dcrossley@indexgeo.com.au>
2016-01-28 08:39:05 +1100

    Enable the alternative filename extension .markdown

--- markdown-include.js
+++ markdown-include.js
@@ -12,8 +12,8 @@ var q = require('q');
 
 this.ignoreTag = ' !ignore';
 this.headingTag = ' !heading';
-this.includePattern = /^#include\s"(.+\/|\/|\w|-|\/)+.md"/gm;
-this.ignorePattern = new RegExp('^#include\\s"(.+\\/|\\/|\\w|-|\\/)+.md"' + this.ignoreTag, 'gm');
+this.includePattern = /^#include\s"(.+\/|\/|\w|-|\/)+\.(md|markdown)"/gm;
+this.ignorePattern = new RegExp('^#include\\s"(.+\\/|\\/|\\w|-|\\/)+\.(md|markdown)"' + this.ignoreTag, 'gm');
 this.headingPattern = new RegExp('^#+\\s.+' + this.headingTag, 'gm');
 this.tableOfContents = '';
 this.build = {};
